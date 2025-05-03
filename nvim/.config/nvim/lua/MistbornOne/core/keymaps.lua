0d9970ed0a795deadc439cc9a24f9b57ebe41eb7
MistbornOne <iankwatkins@gmail.com>
2025-05-02 20:24:43 -0400

    add keymaps for moving left and right between windows

--- nvim/.config/nvim/lua/MistbornOne/core/keymaps.lua
+++ nvim/.config/nvim/lua/MistbornOne/core/keymaps.lua
@@ -24,6 +24,8 @@ keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) --
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sl", "<C-w>h", { desc = "Move left one window" }) -- move to the left window
+keymap.set("n", "<leader>sr", "<C-w>l", { desc = "Move right one window" }) -- move to the right window
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
