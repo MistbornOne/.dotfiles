417371713ba63bc5742f91bf1d338dedefeda0b8
MistbornOne <iankwatkins@gmail.com>
2026-05-14 08:28:53 -0400

    Added Coffee and Commits to Blog.sh

--- scripts/scripts/blog.sh
+++ scripts/scripts/blog.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 BEAR_BLOG_PATH=/Users/ianwatkins/dev/github/MistbornOne/blog/bear/
+COFFEE_BLOG_PATH=/Users/ianwatkins/dev/github/MistbornOne/blog/coffee_commits/
 DEV_BLOG_PATH=/Users/ianwatkins/dev/github/MistbornOne/blog/ian-watkins/
 TERM_BLOG_PATH=/Users/ianwatkins/dev/github/MistbornOne/blog/terminal/
 STORY_BLOG_PATH=/Users/ianwatkins/dev/github/MistbornOne/blog/stories/
@@ -12,18 +13,20 @@ if ! command -v gum &> /dev/null; then
 fi
 
 echo "$(gum style --foreground \#b48ead "Which site?:")"
-site_type=$(gum choose --height 4 "bear" "ian-watkins" "terminal" "stories")
+site_type=$(gum choose --height 5 "Bear" "Coffee & Commits" "Ian-Watkins" "Terminal" "Stories")
 
 echo "$(gum style --foreground \#b48ead "Sync GitHub?")"
   sync_github=$(gum choose --height 2 "yes" "no")
 
-if [[ $site_type == "bear" ]]; then
+if [[ $site_type == "Bear" ]]; then
     blog_path=$BEAR_BLOG_PATH
-elif [[ $site_type == "ian-watkins" ]]; then
+elif [[ $site_type == "Coffee & Commits" ]]; then
+    blog_path=$COFFEE_BLOG_PATH
+elif [[ $site_type == "Ian-Watkins" ]]; then
     blog_path=$DEV_BLOG_PATH
-elif [[ $site_type == "terminal" ]]; then
+elif [[ $site_type == "Terminal" ]]; then
     blog_path=$TERM_BLOG_PATH
-elif [[ $site_type == "stories" ]]; then
+elif [[ $site_type == "Stories" ]]; then
     blog_path=$STORY_BLOG_PATH
 else
     echo "Invalid selection."
